b9bdefccba94f5bb7e8fa2d1d5bd5aab049361d1
zhangyunan <zyndev@gmail.com>
2021-04-23 19:36:27 +0800

    增加日志

--- drools-single-demo/src/test/java/com/github/sbb/drools/HelloRuleTest.java
+++ drools-single-demo/src/test/java/com/github/sbb/drools/HelloRuleTest.java
@@ -4,20 +4,21 @@ import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloRuleTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(HelloRuleTest.class);
+
 
   @Test
   public void testHelloRule() {
     KieServices kss = KieServices.Factory.get();
     KieContainer kc = kss.getKieClasspathContainer();
-
     KieSession ks = kc.newKieSession();
-
     int i = ks.fireAllRules();
-
-    System.out.println("总共执行次数" + i);
-
+    logger.info("总共执行次数 {}", i);
     ks.dispose();
 
   }
